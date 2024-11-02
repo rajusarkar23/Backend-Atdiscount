64ed36e8d430aca836e784d25673e8fa3dfd641a
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-02 15:24:19 +0530

    complete register controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,38 @@
+import { User } from "../models/user.model";
+import bcryptjs from "bcryptjs"
+import { generateOtp } from "../utils";
+import jwt from "jsonwebtoken"
+
 // Register
-const register = (req: any, res: any) => {
+const register = async (req: any, res: any) => {
     const {fullName, email, password} = req.body;
     if (!fullName || !email || !password) {
         return res.status(400).json({success: false, message: "All fields are rquired"})
     }
 
-    // const findUserByEmail = await
+    const findUserByEmail = await User.findOne({email})
+
+    if (findUserByEmail) {
+        return res.status(406).json({success: false, message: "User already available with this email."})
+    }
+
+    const otp = generateOtp()
+    const hashedOtp = bcryptjs.hashSync(otp, 10)
+    
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const createUser = await User.create({fullName, email, password: hashedPassword, otp: hashedOtp})
+
+    if (!createUser) {
+        return res.status(500).json({success: false, message: "Internal server error"})
+    }
+
+    const createdUser = await User.findById(createUser._id).select("-password -otp")
+
+    const jwt_token = jwt.sign({userId: createUser._id}, `${process.env.EMAIL_JWT_SECRET}`, {expiresIn: "60m"})
+
+    return res.cookie("otpVerifyToken", jwt_token).status(201).json({success: true, message: "User created successfully.", createdUser, jwt_token})
 }
 
 // Login
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,11 @@
 export const DB_Name = "at-discount"
+
+// generate otp function
+
+export function generateOtp(l=6) {
+    let otp = ""
+    for (let i = 0; i < l; i++) {
+        otp += Math.floor(Math.random() * 10)
+    }
+    return otp
+}
